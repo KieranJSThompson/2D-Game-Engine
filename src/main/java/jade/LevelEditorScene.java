0884ee035a32d43ac22e1267192bcf2b54553142
Kieran Thompson <krizzle2home@gmail.com>
2022-05-07 16:24:56 +0100

    cleaned up LevelEditorScene, removed default vertex and fragment source.

--- src/main/java/jade/LevelEditorScene.java
+++ src/main/java/jade/LevelEditorScene.java
@@ -12,28 +12,6 @@ import static org.lwjgl.opengl.ARBVertexArrayObject.glGenVertexArrays;
 import static org.lwjgl.opengl.GL20.*;
 
 public class LevelEditorScene extends Scene {
-    private String vertexShaderSource = "#version 330 core\n" +
-            "layout (location=0) in vec3 aPos;\n" +
-            "layout (location=1) in vec4 aColor;\n" +
-            "\n" +
-            "out vec4 fColor;\n" +
-            "\n" +
-            "void main() {\n" +
-            "    fColor = aColor;\n" +
-            "    gl_Position = vec4(aPos, 1.0);\n" +
-            "}\n";
-
-    private String fragmentShaderSource = "#version 330 core\n" +
-            "\n" +
-            "in vec4 fColor;\n" +
-            "\n" +
-            "out vec4 color;\n" +
-            "\n" +
-            "void main() {\n" +
-            "    color = fColor;\n" +
-            "}";
-
-    private int vertexId, fragmentId, shaderProgram;
     private float[] vertexArray = {
         // position         // color
         100.5f, -0.5f, 0.5f,  1.0f, 0.0f, 0.0f, 1.0f, // bottom right
